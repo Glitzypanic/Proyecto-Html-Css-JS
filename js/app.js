127a713ab8fb5d30615a04f650858d0c12ce9370
Glitzypanic <jose.fariasdv@gmail.com>
2023-11-13 12:39:09 -0300

    Comienzo de segunda pagina de juego

--- js/app.js
+++ js/app.js
@@ -1,2 +1,25 @@
+// Inicializacion de var, objetos, DOM
+const nickInput = document.getElementById('nick')
+const tamanoInput = document.getElementById('tamano')
+const formEntrada = document.getElementById('formEntrada')
+
+// Funciones de eventos
+function comprobarForm(event) {
+    if (nickInput.value.length==0) {
+        console.log('No hay nick');
+        nickInput.focus()
+        event.preventDefault()
+        return false
+    }   else if (tamanoInput.value=='0') {
+        console.log('No se ha seleccionado tamano de juego');
+        tamanoInput.focus()
+        event.preventDefault()
+        return false
+    }
+    return true
+}
+
+// Inicio de carga de eventos
+formEntrada.addEventListener('submit', comprobarForm)
 
 
