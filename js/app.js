172c397e2bb98cc71b6d0da9e677f44d89c9bc77
Glitzypanic <jose.fariasdv@gmail.com>
2023-11-15 14:44:33 -0300

    Listo para comenzar el juego

--- js/app.js
+++ js/app.js
@@ -2,21 +2,25 @@
 const nickInput = document.getElementById('nick')
 const tamanoInput = document.getElementById('tamano')
 const formEntrada = document.getElementById('formEntrada')
+const error = document.getElementById('error')
+
 
 // Funciones de eventos
 function comprobarForm(event) {
-    if (nickInput.value.length==0) {
-        console.log('No hay nick');
+    if (nickInput.value.match(/((?<!\S))[0-9]/)) {
+        
         nickInput.focus()
         event.preventDefault()
+        error.innerText = "El campo de nick no puede comenzar con un numero"
         return false
-    }   else if (tamanoInput.value=='0') {
-        console.log('No se ha seleccionado tamano de juego');
+    }else if (tamanoInput.value=='0') {
+                
         tamanoInput.focus()
         event.preventDefault()
+        error.innerText = "Se debe seleccionar un tamano de panel"
         return false
     }
-    return true
+        return true
 }
 
 // Inicio de carga de eventos
